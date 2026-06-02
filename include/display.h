e8c9c8e6c457b91887c230cc30c972acfd742cd3
Nanat21 <nanatabeshu97@gmail.com>
2026-06-02 21:33:55 +0300

    Add function prototypes for printing metrics and charts

--- include/display.h
+++ include/display.h
@@ -3,14 +3,20 @@
 
 #include "input.h"
 
+// Gantt chart event (process execution segment)
 typedef struct {
     int pid;
     int start;
     int end;
 } GanttEvent;
 
+// Prints process table
 void printTable(Process processes[], int n);
+
+// Prints Gantt chart
 void printGanttChart(GanttEvent gantt[], int count);
+
+// Prints performance summary metrics
 void printSummary(float avgWT, float avgTAT, float avgRT, float cpuUtil, float throughput);
 
 #endif
